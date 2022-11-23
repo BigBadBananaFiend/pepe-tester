c8d91a81bd90cceaed8899b201cd3070901b8f54
BigBadBananaFiend <92824002+BigBadBananaFiend@users.noreply.github.com>
2022-11-23 11:48:29 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { AppRoutes } from "./Routes";
 import Smartlook from "smartlook-client";
 
 function App() {
-  Smartlook.init("32d0da1e0539830e04cee71dd9eaeb0fca8c04c1");
+  Smartlook.init("beta_32d0da1e0539830e04cee71dd9eaeb0fca8c04c1");
 
   return (
     <>
